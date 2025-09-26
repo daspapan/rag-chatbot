e5546359d75cbdbd69e631566b110e239ea78490
Papan Das <167051110+daspapan@users.noreply.github.com>
2025-09-26 15:33:03 +0530

    infra setup

---
+++ infra/bin/infra.ts
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import * as gitBranch from 'git-branch';
+import { CDKContext } from '../types';
+
+
+const app = new cdk.App();
+
+const currentBranch = process.env.AWS_BRANCH || gitBranch.sync();
+// console.log(`Deploying on branch -> ${currentBranch}`);
+const globals = app.node.tryGetContext('globals') || {}
+// console.log(`Globals -> ${JSON.stringify(globals)}`);
+const branchConfig = app.node.tryGetContext(currentBranch);
+console.log(`Branch config -> ${JSON.stringify(branchConfig)}`);
+
+if(!branchConfig){
+    throw new Error(`No configuration found for branch: ${currentBranch}`)
+}
+
+const context: CDKContext & cdk.StackProps = {
+    branch: currentBranch,
+    ...globals,
+    ...branchConfig
+}
+
+console.log(`Context -> ${JSON.stringify(context)}`);
+
+const appName = `${context.appName}-${context.stage}`
+// const stackName = `${appName}-Stack`
+
+/* const kbStack = new KnowledgeBaseStack(
+    app, 
+    `${appName}-KBStack`, 
+    {
+        stackName: `${appName}-KBStack`,
+        description: 'AWS Sample Code (uksb-7mdw5l0lhh)'
+    },
+    context
+) */
+
+
